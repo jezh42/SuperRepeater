877fab5ebc2ab71c1204b6818a3bcbc6adfdc8d5
Jez <jhamann42@gmail.com>
2021-10-09 19:58:58 +1100

    Send functionality setup

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -2,6 +2,8 @@ package burp;
 
 import java.awt.*;
 import java.awt.event.MouseAdapter;
+import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import javax.swing.*;
 import java.awt.event.MouseEvent;
@@ -10,9 +12,13 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, IMessageEditorController {
+
+    // Debug
+    private boolean showBorders = false;
 
     // Burp Extender Inits
     private final String extensionName = "Super Repeater";
@@ -30,6 +36,8 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
     private IMessageEditor requestViewer;
     private IMessageEditor responseViewer;
     private JPanel inspectorViewer;
+    private JLabel httpServiceLabel;
+
 
     // Buttons
     JButton sendSplitButton;
@@ -37,8 +45,6 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
 
     // HTTP
     private IHttpRequestResponse currentlyDisplayedItem;
-
-
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
@@ -52,6 +58,9 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
         callbacks.registerContextMenuFactory(this);
         // this.callbacks.registerExtensionStateListener(this);
 
+        // Initialize the currently displayed item
+        resetCurrentlyDisplayedItem();
+
         // Set up the Swing stuff
         SwingUtilities.invokeLater(new Runnable()
         {
@@ -59,50 +68,71 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
             public void run()
             {
                 contentPane = new JPanel(new GridBagLayout());
-                GridBagConstraints constraints = new GridBagConstraints();
 
+                stdout.println("Loading Menu Pane...");
                 // Menu Buttons
                 makeMenuPane();
+                stdout.println("Menu Pane Loaded");
 
                 // Request
+                stdout.println("Loading Request Pane...");
+                String requestString = "Request";
                 requestViewer = callbacks.createMessageEditor(BurpExtender.this, true);
+                JPanel requestPane = makeMessageEditorPane(requestString, requestViewer);
+                if (showBorders) { requestPane.setBorder(BorderFactory.createLineBorder(Color.BLACK)); }
+                stdout.println("Request Pane Loaded");
 
                 // Response
+                stdout.println("Loading Response Pane...");
+                String responseString = "Response";
                 responseViewer = callbacks.createMessageEditor(BurpExtender.this, false);
+                JPanel responsePane = makeMessageEditorPane(responseString, responseViewer);
+                if (showBorders) {  responsePane.setBorder(BorderFactory.createLineBorder(Color.BLACK)); }
+                responseViewer.getComponent().setVisible(false);
+                stdout.println("Response Pane Loaded");
 
                 // Inspector
+                stdout.println("Loading Inspector Pane...");
                 inspectorViewer = new JPanel();
                 JLabel inspectorLabel = new JLabel("Inspector");
                 inspectorViewer.add(inspectorLabel);
+                stdout.println("Inspector Pane Loaded");
 
+                stdout.println("Loading ThreePanelContainer Pane...");
                 threePanelContainer = new JPanel(new GridLayout(1,3));
-                threePanelContainer.add(requestViewer.getComponent());
-                threePanelContainer.add(responseViewer.getComponent());
+                threePanelContainer.add(requestPane);
+                threePanelContainer.add(responsePane);
                 threePanelContainer.add(inspectorViewer);
+                stdout.println("ThreePanelContainer Pane Loaded");
 
                 // Add everything to the Content GridBagLayout
-                constraints.fill = GridBagConstraints.HORIZONTAL;
-                constraints.anchor = GridBagConstraints.PAGE_START;
-                //constraints.ipadx = 0;
-                constraints.ipady = 20;
-                constraints.weightx = 1.0;
-                constraints.weighty = 0;
-                constraints.gridx = 0;
-                constraints.gridy = 0;
-                contentPane.add(menuPane, constraints);
+                stdout.println("Adding Menu Pane to ContentPane...");
+                GridBagConstraints menuPaneConstraints = new GridBagConstraints();
+                menuPaneConstraints.fill = GridBagConstraints.HORIZONTAL;
+                menuPaneConstraints.anchor = GridBagConstraints.PAGE_START;
+                menuPaneConstraints.weightx = 1.0;
+                menuPaneConstraints.weighty = 0;
+                menuPaneConstraints.gridx = 0;
+                menuPaneConstraints.gridy = 0;
+                contentPane.add(menuPane, menuPaneConstraints);
+                stdout.println("Menu Pane added to ContentPane");
 
-                constraints.fill = GridBagConstraints.BOTH;
-                //constraints.anchor =
-                //constraints.ipadx = 0;
-                //constraints.ipady = GRID;
-                constraints.weightx = 1.0;
-                constraints.weighty = 1.0;
-                constraints.gridx = 0;
-                constraints.gridy = 1;
-                contentPane.add(threePanelContainer, constraints);
+                stdout.println("Adding ThreePanelContainer to ContentPane...");
+                GridBagConstraints threePanelConstraints = new GridBagConstraints();
+                threePanelConstraints.fill = GridBagConstraints.BOTH;
+                threePanelConstraints.weightx = 1.0;
+                threePanelConstraints.weighty = 1.0;
+                threePanelConstraints.gridx = 0;
+                threePanelConstraints.gridy = 1;
+                contentPane.add(threePanelContainer, threePanelConstraints);
+                stdout.println("ThreePanelContainer added to ContentPane");
 
                 // Add new tab to Burp
+                stdout.println("Adding SuiteTab to BurpSuite...");
                 callbacks.addSuiteTab(BurpExtender.this);
+
+                stdout.println("Extension fully loaded");
+                stdout.println("**************************************************\n");
             }
         });
     }
@@ -119,7 +149,7 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
         JMenuItem sendMenuSendTenTimes = new JMenuItem("Send x10");
         sendMenuSendTenTimes.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                cancelButton.setEnabled(false);
+                sendRequest(10);
             }
         });
         sendPopupMenu.add(sendMenuSendTenTimes);
@@ -128,7 +158,7 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
         JMenuItem sendMenuSendTwentyTimes = new JMenuItem("Send x20");
         sendMenuSendTwentyTimes.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                cancelButton.setEnabled(false);
+                sendRequest(20);
             }
         });
         sendPopupMenu.add(sendMenuSendTwentyTimes);
@@ -142,8 +172,7 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
 
                 if (mouseX < 50 ) {
                     // Perform Send if clicks the Send word
-                    cancelButton.setEnabled(true);
-                    // Send the request
+                    sendRequest(1);
 
                 } else if (mouseX > 50) {
                     // Display the dropdown if clicks the *
@@ -157,6 +186,63 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
         });
 
         return sendSplitButton;
+    }
+
+    /**
+     *
+     * @param requestCount
+     */
+    private void sendRequest(int requestCount) {
+
+        stdout.println("Sending requests...");
+
+        this.cancelButton.setEnabled(true);
+
+        for (int i = 0; i < requestCount; i++) {
+
+            // If cancel is clicked
+            if (!cancelButton.isEnabled()) {
+                stdout.println("Cancel was clicked during send loop...");
+                break;
+            }
+
+            IHttpService httpService = this.getHttpService();
+            byte[] request = this.getRequest();
+            stdout.println("Trying to send request [" + i + "]");
+
+            // Reset the response
+            // TODO make this an listener that checks for empty content
+            this.responseViewer.getComponent().setVisible(false);
+
+//            if (httpService.getProtocol() != "https") {
+//                // Display message
+//                JOptionPane.showMessageDialog(
+//                        null,
+//                        "Error: Only HTTPS is supported in the current version of Super Repeater",
+//                        "Super Repeater Error",
+//                        JOptionPane.INFORMATION_MESSAGE
+//                );
+//
+//                break;
+//            }
+
+            if (httpService != null && request != null){
+                try {
+                    stdout.println("   [" + i + "] to " + httpService.getHost());
+                    this.currentlyDisplayedItem = callbacks.makeHttpRequest(httpService, request);
+                    this.responseViewer.getComponent().setVisible(true);
+                } catch (Exception e) {
+                    stderr.println("Error: " + e.toString());
+                }
+
+                stdout.println("Request [" + i + "] sent");
+            }
+
+        }
+
+        stdout.println("All Requests sent");
+
+        this.cancelButton.setEnabled(false);
     }
 
     /**
@@ -180,28 +266,96 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
 
     /**
      * Make the menu pane with buttons
+     * TODO: Make it a GridBagLayout to make it look nicer
      */
     private void makeMenuPane() {
 
         // Left Side of the Menu
+        stdout.println("Loading menuPaneLeft...");
         JPanel menuPaneLeft = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        if (showBorders) { menuPaneLeft.setBorder(BorderFactory.createLineBorder(Color.BLUE)); }
         this.sendSplitButton = makeSendSplitButton();
         this.cancelButton = makeCancelButton();
         menuPaneLeft.add(sendSplitButton);
         menuPaneLeft.add(cancelButton);
+        stdout.println("menuPaneLeft Loaded");
 
         // Right Side of the Menu
+        stdout.println("Loading menuPaneRight...");
         JPanel menuPaneRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        JButton testButton = new JButton("Test button");
-        menuPaneRight.add(testButton);
+        if (showBorders) { menuPaneRight.setBorder(BorderFactory.createLineBorder(Color.RED)); }
+        // Label
+        httpServiceLabel = new JLabel("Target: Not Specified");
+        updateHttpServiceLabel(this.getHttpService());
+        menuPaneRight.add(httpServiceLabel);
+        stdout.println("menuPaneRight Loaded");
 
         // Add them to the menu
+        stdout.println("Combining menuPane...");
         this.menuPane = new JPanel(new GridLayout(1,2));
         this.menuPane.add(menuPaneLeft);
         this.menuPane.add(menuPaneRight);
+        stdout.println("menuPane combined");
+
+        if (showBorders) { this.menuPane.setBorder(BorderFactory.createLineBorder(Color.GREEN)); }
 
         // Set the size of the menu to restrict the height
-        this.menuPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, 5));
+        this.menuPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, 3));
+    }
+
+    /**
+     *
+     */
+    private void updateHttpServiceLabel(IHttpService currentHttpService) {
+        stdout.println("Updating HttpServiceLabel...");
+
+        if (currentHttpService != null) {
+            this.httpServiceLabel.setText("Target: " +
+                    currentHttpService.getProtocol() + "://" +
+                    currentHttpService.getHost() + ":" +
+                    currentHttpService.getPort()
+            );
+        }
+
+        stdout.println("HttpServiceLabel updated");
+    }
+
+    /**
+     *
+     * @param labelString
+     * @param messageEditor
+     * @return
+     */
+    private JPanel makeMessageEditorPane(String labelString, IMessageEditor messageEditor){
+        JPanel messageEditorPane = new JPanel(new GridBagLayout());
+
+        JLabel label = new JLabel(labelString);
+        GridBagConstraints labelConstraints = new GridBagConstraints();
+        labelConstraints.fill = GridBagConstraints.HORIZONTAL;
+        labelConstraints.anchor = GridBagConstraints.PAGE_START;
+        labelConstraints.ipady = 20;
+        labelConstraints.weightx = 1.0;
+        labelConstraints.weighty = 0;
+        labelConstraints.gridx = 0;
+        labelConstraints.gridy = 0;
+        messageEditorPane.add(label, labelConstraints);
+
+        JPanel messageEditorContainer = new JPanel(new GridBagLayout());
+        GridBagConstraints messageEditorContainerConstraints = new GridBagConstraints();
+        messageEditorContainerConstraints.fill = GridBagConstraints.BOTH;
+        messageEditorContainerConstraints.weightx = 1.0;
+        messageEditorContainerConstraints.weighty = 1.0;
+        messageEditorContainer.add(messageEditor.getComponent(), messageEditorContainerConstraints);
+
+        GridBagConstraints messageEditorConstraints = new GridBagConstraints();
+        messageEditorConstraints.fill = GridBagConstraints.BOTH;
+        messageEditorConstraints.weightx = 1.0;
+        messageEditorConstraints.weighty = 1.0;
+        messageEditorConstraints.gridx = 0;
+        messageEditorConstraints.gridy = 1;
+        messageEditorPane.add(messageEditorContainer, messageEditorConstraints);
+
+        return messageEditorPane;
     }
 
     @Override
@@ -229,6 +383,77 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
         return currentlyDisplayedItem.getResponse();
     }
 
+    /**
+     * Reset CurrentlyDisplayedItem to empty values
+     */
+    private void resetCurrentlyDisplayedItem() {
+
+        stdout.println("Resetting currentlyDisplayedItem...");
+
+        this.currentlyDisplayedItem = new IHttpRequestResponse() {
+            @Override
+            public byte[] getRequest() {
+                return new byte[0];
+            }
+
+            @Override
+            public void setRequest(byte[] message) {
+
+            }
+
+            @Override
+            public byte[] getResponse() {
+                return new byte[0];
+            }
+
+            @Override
+            public void setResponse(byte[] message) {
+
+            }
+
+            @Override
+            public String getComment() {
+                return null;
+            }
+
+            @Override
+            public void setComment(String comment) {
+
+            }
+
+            @Override
+            public String getHighlight() {
+                return null;
+            }
+
+            @Override
+            public void setHighlight(String color) {
+
+            }
+
+            @Override
+            public IHttpService getHttpService() {
+                return null;
+            }
+
+            @Override
+            public void setHttpService(IHttpService httpService) {
+
+            }
+        };
+
+        byte[] fakeReq = {};
+        this.currentlyDisplayedItem.setRequest(new byte[0]);
+
+        stdout.println("currentlyDisplayedItem reset!");
+        stdout.println("currentlyDisplayedItem.getRequest(): " +
+                helpers.bytesToString(this.currentlyDisplayedItem.getRequest())
+        );
+        stdout.println("currentlyDisplayedItem.getResponse(): " +
+                helpers.bytesToString(this.currentlyDisplayedItem.getResponse())
+        );
+    }
+
     @Override
     public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
         List<JMenuItem> contextMenuList = new ArrayList<>();
@@ -240,14 +465,28 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
                 IHttpRequestResponse clickedItem = invocation.getSelectedMessages()[0];
                 byte[] request = clickedItem.getRequest();
                 byte[] response = clickedItem.getResponse();
-                stdout.println("Request:" + helpers.bytesToString(request));
-                stdout.println("Response:" + helpers.bytesToString(response));
-                if (request.length != 0){
+                IHttpService httpService = clickedItem.getHttpService();
+
+                if (request != null && request.length != 0){
+                    stdout.println("Request Length:" + request.length);
+                    //stdout.println("Request:" + helpers.bytesToString(request));
+                    stdout.println("Request: " + Arrays.toString(request));
                     requestViewer.setMessage(request, true);
                 }
-                if (response.length != 0){
+
+                /* Decided to change it so won't load the response
+                if (response != null && response.length != 0){
+                    stdout.println("Response Length:" + response.length);
+                    stdout.println("Response:" + helpers.bytesToString(response));
                     responseViewer.setMessage(response, false);
                 }
+                */
+
+                if (httpService != null) {
+                    stdout.println("Setting HTTP Service");
+                    updateHttpServiceLabel(httpService);
+                }
+
                 currentlyDisplayedItem = clickedItem;
             }
         });
@@ -255,7 +494,7 @@ public class BurpExtender implements IContextMenuFactory, IBurpExtender, ITab, I
         // If what being selected is a request that can be sent to Super Repeater
         //   then show the context menu item
         byte contextCode = invocation.getInvocationContext();
-        //stdout.println("Context: " + contextCode);
+        stdout.println("Context: " + contextCode);
         if (contextCode == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST ||
                 contextCode == IContextMenuInvocation.CONTEXT_PROXY_HISTORY ||
                 contextCode == IContextMenuInvocation.CONTEXT_TARGET_SITE_MAP_TABLE ||
